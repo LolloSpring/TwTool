87406c4ea1ccef8dd7b1f92f50ac9ab1a5a9eeb6
Lorenzo Tait <lorenzo.tait95@gmail.com>
2016-04-17 18:40:22 +0200

    Aggiungo requisito 04

--- twitter.h
+++ twitter.h
@@ -1,14 +1,28 @@
-//
-//  twitter.h
-//  
-//
-//  Created by Lorenzo Tait on 15/04/16.
-//
-//
 
 #ifndef ____twitter__
 #define ____twitter__
 
 #include <stdio.h>
 
+#include <iostream>
+
+const int dim=256;
+
+
+void concatena (const char a[], const char estensione[], char c[], int len_a, int len_b);
+void saluto ();
+void get_data (char data[dim]);
+char get_scelta ();
+
+
+
 #endif /* defined(____twitter__) */
+
+
+//
+//  twitter.h
+//
+//
+//  Created by Lorenzo Tait on 15/04/16.
+//
+//
